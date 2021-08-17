63d1ea45811211720e566686ece8d6b5f74628c8
skjaved633 <skjaved633@gmail.com>
2021-08-17 21:48:55 +0530

    home

--- test_1/src/Components/Home.jsx
+++ test_1/src/Components/Home.jsx
@@ -2,17 +2,26 @@ import React, { useState } from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import { ToDoList } from "./ToDoList";
 import { AddToDo } from "./AddToDo";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export const Home = () => {
+  const [toDoItems, setToDoItems] = useState([]);
+  const handleAdd = (newToDo) => {
+    setToDoItems([...toDoItems, newToDo]);
+  };
+  const handleDelete = (curritem) => {
+    const filtered = toDoItems.filter((item) => item !== curritem);
+    setToDoItems([...filtered]);
+  };
   return (
     <Container>
       <Row>
         <Col>
           <Card>
-            <AddToDo />
+            <AddToDo handleAdd={handleAdd} />
           </Card>
           <Card>
-            <ToDoList />
+            <ToDoList toDoItems={toDoItems} handleDelete={handleDelete} />
           </Card>
         </Col>
       </Row>

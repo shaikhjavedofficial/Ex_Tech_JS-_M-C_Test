ace238ec718d8d307ce48e1d29de6796c4c25009
skjaved633 <skjaved633@gmail.com>
2021-08-17 23:26:40 +0530

    class rm

--- test_1/src/Components/ToDoList.jsx
+++ test_1/src/Components/ToDoList.jsx
@@ -3,7 +3,7 @@ import { Card } from "react-bootstrap";
 import { ToDoItems } from "./ToDoItems";
 export const ToDoList = ({ toDoItems, handleDelete }) => {
   return (
-    <React.Fragment className="todolist">
+    <React.Fragment>
       {toDoItems.map((item) => (
         <ToDoItems item={item} handleDelete={handleDelete} />
       ))}

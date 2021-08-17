bc20fb31dc5dd77491698e8f1b555aa82576ffc1
skjaved633 <skjaved633@gmail.com>
2021-08-17 21:31:59 +0530

    todolist

--- test_1/src/Components/ToDoList.jsx
+++ test_1/src/Components/ToDoList.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 import { ToDoItems } from "./ToDoItems";
-export const ToDoList = () => {
+export const ToDoList = ({ toDoItems, handleDelete }) => {
   return (
     <React.Fragment>
-      <ToDoItems />
+      <Card className="todolist">
+        {toDoItems.map((item) => (
+          <ToDoItems item={item} handleDelete={handleDelete} />
+        ))}
+      </Card>
     </React.Fragment>
   );
 };
